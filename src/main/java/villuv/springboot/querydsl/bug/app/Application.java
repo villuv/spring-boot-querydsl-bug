522a6f9c3eb4162c5e56bfed8a487f14e8206920
Villu Vaimets <villu.vaimets@resys.io>
2015-06-06 15:48:27 +0300

    Removed unused @ImportResource

--- src/main/java/villuv/springboot/querydsl/bug/app/Application.java
+++ src/main/java/villuv/springboot/querydsl/bug/app/Application.java
@@ -2,10 +2,8 @@ package villuv.springboot.querydsl.bug.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath*:spring/*-context.xml"})
 public class  Application {
 
   public static void main(String[] args) {
